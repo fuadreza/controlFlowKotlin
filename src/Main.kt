32986716099e9ee428612ebb7d305c8e62f46c8f
fuadreza <fuadreza1011@gmail.com>
2018-09-27 22:19:52 +0700

    second commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
     for (i in 1..100) {
-        println(when(0){
-            i%10 -> "$i -> angka kelipatan 10"
-            i%2 -> "$i -> angka genap"
+        println(when (0) {
+            i % 10 -> "$i -> angka kelipatan 10"
+            i % 2 -> "$i -> angka genap"
             else -> i
         })
     }
